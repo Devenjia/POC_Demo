b912def76b67278dddd000981ed3c1ad2096af5c
jiayuzhuo <1003398530@qq.com>
2017-10-12 16:31:36 +0800

    一条鱼

---
+++ report/autoreport.js
@@ -0,0 +1,53 @@
+$(function () {
+
+    $('#table').bootstrapTable({
+        classes: 'table table-hover',
+        striped:true,
+        data: [
+            {
+                id: 1,
+                name: 'Item 1',
+                price: '$1'
+            }, {
+                id: 2,
+                name: 'Item 2',
+                price: '$2'
+            }, {
+                id: 1,
+                name: 'Item 1',
+                price: '$1'
+            }, {
+                id: 2,
+                name: 'Item 2',
+                price: '$2'
+            }, {
+                id: 1,
+                name: 'Item 1',
+                price: '$1'
+            }, {
+                id: 2,
+                name: 'Item 2',
+                price: '$2'
+            }, {
+                id: 1,
+                name: 'Item 1',
+                price: '$1'
+            }, {
+                id: 2,
+                name: 'Item 2',
+                price: '$2'
+            }, {
+                id: 1,
+                name: 'Item 1',
+                price: '$1'
+            }, {
+                id: 2,
+                name: 'Item 2',
+                price: '$2'
+            }
+
+
+        ]
+    });
+});
+
