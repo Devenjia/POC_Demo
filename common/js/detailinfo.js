19c82187a3c1003b43cef217201311ab61ca241c
Li <yugang.li@accenture.com>
2017-10-13 17:06:41 +0800

    sdf

---
+++ common/js/detailinfo.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-10-13 16:59:59
+ * @version $Id$
+ */
+$(function () {
+	$('#table').bootstrapTable({
+	    classes: 'table table-hover',
+	    striped: true,
+	    data: jsonData
+	})
+});
